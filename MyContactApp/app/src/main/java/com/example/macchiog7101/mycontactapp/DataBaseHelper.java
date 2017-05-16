d8dae5799e4f3eb1d465934aa24d6e99f4124620
Frogman1117 <gregory.macchio@gmail.com>
2017-05-16 10:36:04 -0700

    5/16/17 Greg Macchio
    I was absent on Mondy thats why I did not push then. This covers Monday
    and Tuesday pushes.

--- MyContactApp/app/src/main/java/com/example/macchiog7101/mycontactapp/DataBaseHelper.java
+++ MyContactApp/app/src/main/java/com/example/macchiog7101/mycontactapp/DataBaseHelper.java
@@ -1,8 +1,11 @@
 package com.example.macchiog7101.mycontactapp;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 /**
  * Created by macchiog7101 on 5/12/2017.
@@ -13,6 +16,8 @@ public class DataBaseHelper extends SQLiteOpenHelper{
     public static final String TABLE_NAME = "contact_table";
     public static final String COL_1 = "ID";
     public static final String COL_2 = "Name";
+    public static final String COL_3 = "Age";
+    public static final String COL_4 = "Address";
 
     public DataBaseHelper(Context context){
         super(context,DATABASE_NAME, null, 1);
@@ -27,5 +32,22 @@ public class DataBaseHelper extends SQLiteOpenHelper{
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion){
         db.execSQL("DROP TABLE IF EXISTS" + TABLE_NAME);
         onCreate(db);
+    }
+
+    public boolean insertData(String name, String age, String address){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL_2, name);
+        contentValues.put(COL_3, age);
+        contentValues.put(COL_4, address);
+
+        long result = db.insert(TABLE_NAME, null, contentValues);
+        if (result == -1) return false;
+        else return true;
+    }
+    public Cursor getAllData(){
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor res = db.rawQuery("select * from" + TABLE_NAME, null);
+        return res;
     }
 }
--- MyContactApp/app/src/main/java/com/example/macchiog7101/mycontactapp/MainActivity.java
+++ MyContactApp/app/src/main/java/com/example/macchiog7101/mycontactapp/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.macchiog7101.mycontactapp;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -9,6 +13,8 @@ public class MainActivity extends AppCompatActivity {
 
     DataBaseHelper myDb;
     EditText editName;
+    EditText editAge;
+    EditText editAddress;
     Button btnAddDate;
 
     @Override
@@ -17,5 +23,32 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         myDb = new DataBaseHelper(this);
+
+        editName = (EditText) findViewById(R.id.editText_NAME);
+        editAge = (EditText) findViewById(R.id.editText_AGE);
+        editAddress = (EditText) findViewById(R.id.editText_ADDRESS);
+    }
+
+    public void addData(View v){
+        boolean isInserted = myDb.insertData(editName.getText().toString(),editAge.getText().toString(),editAddress.getText().toString());
+
+        if (isInserted == true){
+            Log.d("MyContact", "Success inserting data");
+        }
+        else {
+            Log.d("MyContact", "Failure inserting data");
+        }
+    }
+    public void viewData(View v){
+        Cursor res = myDb.getAllData();
+        if (res.getCount() == 0){
+            showMessage("Error", "No data is found in the data base");
+            return;
+        }
+        StringBuffer buffer = new StringBuffer();
+        showMessage("Data",buffer.toString());
+    }
+    private void showMessage(String title, String message){
+
     }
 }
