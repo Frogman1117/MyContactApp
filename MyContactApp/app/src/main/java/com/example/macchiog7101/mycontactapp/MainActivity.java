a737b6a389e6e2f5aef7b758902a61c1d3fd4abd
Frogman1117 <gregory.macchio@gmail.com>
2017-05-18 10:30:56 -0700

    5/18/17 Greg Macchio #8

--- MyContactApp/app/src/main/java/com/example/macchiog7101/mycontactapp/MainActivity.java
+++ MyContactApp/app/src/main/java/com/example/macchiog7101/mycontactapp/MainActivity.java
@@ -1,15 +1,14 @@
 package com.example.macchiog7101.mycontactapp;
 
-import android.content.DialogInterface;
 import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,7 +16,9 @@ public class MainActivity extends AppCompatActivity {
     EditText editName;
     EditText editAge;
     EditText editAddress;
-    Button btnAddDate;
+    EditText editSearch;
+    Button btnAddData;
+    String[] fields;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,47 +26,84 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         myDb = new DataBaseHelper(this);
-
-        editName = (EditText) findViewById(R.id.editText_NAME);
-        editAge = (EditText) findViewById(R.id.editText_AGE);
-        editAddress = (EditText) findViewById(R.id.editText_ADDRESS);
+        //add the layout bars
+        editName = (EditText) findViewById(R.id.editText_Name);
+        editAge = (EditText) findViewById(R.id.editText_Age);
+        editAddress = (EditText) findViewById(R.id.editText_address);
+        editSearch = (EditText) findViewById(R.id.editSearch);
+        fields = new String[4];
+        fields[0] = "ID: ";
+        fields[1] = "Name: ";
+        fields[2] = "Age: ";
+        fields[3] = "Address: ";
     }
+    public void addData(View v) {
+        boolean isInserted = (myDb.insertData(editName.getText().toString(),editAge.getText().toString(),editAddress.getText().toString()));
 
-    public void addData(View v){
-        boolean isInserted = myDb.insertData(editName.getText().toString(),editAge.getText().toString(),editAddress.getText().toString());
-
-        if (isInserted == true){
+        String text = "";
+        if (isInserted == true) {
             Log.d("MyContact", "Success inserting data");
+            text = "Data inserted";
+            Toast toast = Toast.makeText(getApplicationContext(), text,Toast.LENGTH_SHORT);
+            toast.show();
         }
         else {
+            text = "Failure";
+            Toast toast = Toast.makeText(getApplicationContext(), text,Toast.LENGTH_SHORT);
+            toast.show();
             Log.d("MyContact", "Failure inserting data");
         }
+        editName.setText("");
+        editAge.setText("");
+        editAddress.setText("");
+
     }
-    public void viewData(View v){
+    public void viewData(View v) {
         Cursor res = myDb.getAllData();
-        if (res.getCount() == 0){
-            showMessage("Error", "No data is found in the data base");
-            return;
+        if (res.getCount() == 0) {
+            showMessage("Error", "No data found in database");
         }
         StringBuffer buffer = new StringBuffer();
-        for(res.moveToFirst();!res.isAfterLast();res.moveToNext()){
-            for (int i =0;i<res.getColumnCount();i++){
-                buffer.append(res.getString(i) + "   ");
+        res.moveToFirst();
+        for (int i = 0; i < res.getCount(); i++) {
+            for (int j = 0; j <= 3; j++) {
+                buffer.append(fields[j]);
+                buffer.append(res.getString(j));
+                buffer.append("\n");
             }
-            buffer.append("\n");
+            res.moveToNext();
         }
-        showMessage("Data",buffer.toString());
-        return;
+        showMessage("Data", buffer.toString());
+        System.out.println(buffer);
     }
-    private void showMessage(String title, String message){
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle(title).setMessage(message)
-                .setPositiveButton("Close", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
 
-                    }
-                });
+    private void showMessage(String title, String message) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(true);
+        builder.setTitle(title);
+        builder.setMessage(message);
         builder.show();
+    }
+    public void searchMessage(View v) {
+        String search = editSearch.getText().toString();
+        StringBuffer buffer = new StringBuffer();
+        Cursor res = myDb.getAllData();
+        res.moveToFirst();
+        for (int i = 0; i < res.getCount(); i++) {
+            if (res.getString(1).equals(search)) {
+                for (int j = 0; j < 4; j++) {
+                    buffer.append(fields[j]);
+                    buffer.append(res.getString(j));
+                    buffer.append("\n");
+                }
+            }
+            res.moveToNext();
+        }
+        if (buffer.toString().equals("")) {
+            showMessage("No results found", "");
+        } else {
+            showMessage("Results", buffer.toString());
+        }
+        editSearch.setText("");
     }
 }
